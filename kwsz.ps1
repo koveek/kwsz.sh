54ac7f5e53bf296051ee94d04ed509faa0f709ab
Patryk Kowejsza <patryk@kowejsza.name>
2024-02-08 11:15:15 +0100

    feat: dynamic script structure

---
+++ kwsz.ps1
@@ -0,0 +1,7 @@
+$ArgsPath = $args -join "\"
+$ScriptDir = "$PSScriptRoot\scripts\$ArgsPath"
+$Script = "$ScriptDir\script.ps1"
+
+if(Test-Path -Path $Script) {
+    & $Script
+}
---
+++ setup.ps1
@@ -0,0 +1,24 @@
+$KwszDir = "$env:USERPROFILE\.kwsz"
+$KwszScript = "$KwszDir\kwsz.ps1"
+
+if(!(Test-Path -Path $KwszDir)) {
+    git clone https://kwsz.sh/scripts $KwszDir
+}
+
+$KwszFunction = @"
+function kwsz {
+    `$currentPolicy = Get-ExecutionPolicy -Scope CurrentUser
+    if (`$currentPolicy -ne 'Bypass') {
+        Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
+    }
+    & $KwszScript @args
+}
+"@
+
+if(!(Test-Path -Path $PROFILE)) {
+    New-Item -Path $PROFILE -ItemType File
+}
+
+if(-not (Get-Content $PROFILE | Select-String "function kwsz")) {
+    Add-Content -Path $PROFILE -Value "`n$KwszFunction"
+}
