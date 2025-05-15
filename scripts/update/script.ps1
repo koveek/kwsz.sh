ff511fac4c027399863aae2be0e9e594f5d7ddf6
Patryk Kowejsza <patryk@kowejsza.name>
2025-05-15 11:59:53 +0200

    feat: return to previous dirctory after update

--- scripts/update/script.ps1
+++ scripts/update/script.ps1
@@ -1,3 +1,4 @@
+$DirectoryBeforeUpdate = Get-Location
 $KwszDir = "$env:USERPROFILE\.kwsz"
 
 Set-Location $KwszDir
@@ -7,5 +8,5 @@ if($LASTEXITCODE -ne 0) { # $LASTEXITCODE of zero typically indicates success, s
     Write-Host "Kwsz could not be updated. Exiting..." -ForegroundColor Red -BackgroundColor Black
     Exit 1
 }
-
+Set-Location $DirectoryBeforeUpdate
 Write-Host $KwszDir
