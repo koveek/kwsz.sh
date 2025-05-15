1d5a41912de996c4aa5a771f9a6d6da70eb3112b
Patryk Kowejsza <patryk@kowejsza.name>
2025-05-15 10:56:38 +0200

    feat: pull alias

---
+++ scripts/update/script.ps1
@@ -0,0 +1,11 @@
+$KwszDir = "$env:USERPROFILE\.kwsz"
+
+Set-Location $KwszDir
+git pull
+
+if($LASTEXITCODE -ne 0) { # $LASTEXITCODE of zero typically indicates success, so it checks if git pull has failed
+    Write-Host "Kwsz could not be updated. Exiting..." -ForegroundColor Red -BackgroundColor Black
+    Exit 1
+}
+
+Write-Host $KwszDir
